99da424f810af365f5db7c417b8a39879306fc7d
sh4869 <nobuk4869@gmail.com>
2016-10-10 15:25:39 +0900

    Introduce React and Setting babel and webpack for React

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,15 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'main.js'
+  },
+  resolve: {
+    root: __dirname + 'src/js'
+  },
+  module: {
+    loaders: [{
+      test:     /\.js$/,
+      exclude:  /node_modules/,
+      loader:  'babel-loader'
+    }]
   }
 };
