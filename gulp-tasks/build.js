6df06cefb86d41e2661d2220d828566b34b54878
sh4869 <nobuk4869@gmail.com>
2016-10-11 11:40:19 +0900

    add webpack-dev-server and remove gulp

--- gulp-tasks/build.js
+++
@@ -1,9 +0,0 @@
-var gulp    = require('gulp');
-var webpack = require('gulp-webpack');
-var config  = require('../webpack.config.js')
-
-gulp.task('build', function () {
-    return gulp.src(config.entry)
-        .pipe(webpack(config))
-        .pipe(gulp.dest("./www/js"));
-});
