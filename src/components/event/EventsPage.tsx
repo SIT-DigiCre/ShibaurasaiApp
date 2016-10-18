b12ca3d06ac9936f369de5025669fc90848507af
sh4869 <nobuk4869@gmail.com>
2016-10-18 18:10:24 +0900

    Create EventPage Element

---
+++ src/components/event/EventsPage.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { EventCard, EventInfo } from "./EventCard";
+import axios from "axios";
+
+interface EventPageState {
+    json: any;
+}
+
+export default class EventPage extends React.Component<{}, EventPageState>{
+    constructor() {
+        super();
+        this.state = { json: "" };
+    }
+    componentDidMount() {
+        axios.get("/src/data/events.json").then(
+            (response) => {
+                this.setState({ json: response.data });
+                console.log(this);
+            }
+        );
+    };
+
+    render() {
+        let cards;
+        if (this.state.json) {
+            cards = (this.state.json as EventInfo[]).map((info, index) => {
+                return (
+                    <EventCard {...info} key={index}/>
+                );
+            });
+        } else {
+            cards = (<div>Now Loading...</div>);
+        }
+        return (
+            <div>
+                {cards}
+            </div>
+        );
+    }
+}
