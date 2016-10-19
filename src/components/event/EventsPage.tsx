18bae4b3406bc3a6c34af6c4b3d5438502f439bf
sh4869 <nobuk4869@gmail.com>
2016-10-19 15:41:23 +0900

    change include method of axios

--- src/components/event/EventsPage.tsx
+++ src/components/event/EventsPage.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { EventCard, EventInfo } from "./EventCard";
-import axios from "axios";
+import * as axios from "axios";
 
 interface EventPageState {
     json: any;
