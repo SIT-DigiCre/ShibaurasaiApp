bbee1776543f796f1a8bab0964a8ced9e4f5dfe1
sh4869 <nobuk4869@gmail.com>
2016-10-30 13:56:03 +0900

    Make DescriptionConverter Singletone

--- src/components/utils/DescriptionConverter.ts
+++ src/components/utils/DescriptionConverter.ts
@@ -1,32 +1,37 @@
 import showdown = require("showdown");
 
 export class DescriptionConverterGenerator {
+    private static _converter = DescriptionConverterGenerator.getConverter();
     static getConverter(): showdown.Converter {
-        const fc2wikibold_extension: { [key: string]: showdown.ShowdownExtension } = {
-            "bold": {
-                type: "lang",
-                regex: /&bold\(\){([^}]*)}/g,
-                replace: (match, prefix) => {
-                    return "<b>" + prefix + "</b>";
+        if (this._converter) {
+            return this._converter;
+        } else {
+            const fc2wikibold_extension: { [key: string]: showdown.ShowdownExtension } = {
+                "bold": {
+                    type: "lang",
+                    regex: /&bold\(\){([^}]*)}/g,
+                    replace: (match, prefix) => {
+                        return "<b>" + prefix + "</b>";
+                    }
+                },
+                "br": {
+                    type: "lang",
+                    regex: /&br;/g,
+                    replace: "<br/>"
+                },
+                "big": {
+                    type: "lang",
+                    regex: /&big\(\){([^}]*)}/g,
+                    replace: (match, prefix) => {
+                        return "Big: " + prefix; // TODO: 文字を大きくする方法を考える
+                    }
                 }
-            },
-            "br": {
-                type: "lang",
-                regex: /&br;/g,
-                replace: "<br/>"
-            },
-            "big": {
-                type: "lang",
-                regex: /&big\(\){([^}]*)}/g,
-                replace: (match, prefix) => {
-                    return "Big: " + prefix; // TODO: 文字を大きくする方法を考える
-                }
+            };
+            const converter = new showdown.Converter();
+            for (let ext_name in fc2wikibold_extension) {
+                converter.addExtension(fc2wikibold_extension[ext_name], ext_name);
             }
-        };
-        const converter = new showdown.Converter();
-        for (let ext_name in fc2wikibold_extension) {
-            converter.addExtension(fc2wikibold_extension[ext_name], ext_name);
+            return converter;
         }
-        return converter;
     }
 }
