c61266439468e140de5fb50d2c211c8ca4ae4263
sh4869 <nobuk4869@gmail.com>
2016-11-01 22:33:50 +0900

    Add React-router path for MapEventList Componet

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import EventPage from "./components/event/EventPage";
 import StagePage from "./components/stage/StagePage";
 import SearchPage from "./components/search/SearchPage";
 import MapPage from "./components/map/MapPage";
+import MapEventList from "./components/map/MapEventList";
 
 import { Router, Route, hashHistory, IndexRoute } from "react-router";
 injectTapEventPlugin();
@@ -21,7 +22,7 @@ class TestElement extends React.Component<Text, {}> {
         return (
             <div className="test">
                 <MenuBar appName="Shibaura Fes Navi" />
-                {this.props.children || <TopPage/>}
+                {this.props.children || <TopPage />}
             </div>
         );
     }
@@ -30,11 +31,12 @@ class TestElement extends React.Component<Text, {}> {
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={TestElement}>
-            <IndexRoute component={TopPage}/>
-            <Route path="/event" component={EventPage}/>
-            <Route path="/stage" component={StagePage}/>
-            <Route path="/search" component={SearchPage}/>
-            <Route path="/map"  component={MapPage}/>
-       </Route>
+            <IndexRoute component={TopPage} />
+            <Route path="/event" component={EventPage} />
+            <Route path="/stage" component={StagePage} />
+            <Route path="/search" component={SearchPage} />
+            <Route path="/map" component={MapPage} />
+            <Route path="/building/:building/:room_or_stall" component={MapEventList} />
+        </Route>
     </Router>
 ), document.getElementById("app"));
